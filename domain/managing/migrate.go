017367764e68dbcff1e88baabc97dd039f53f3f7
Diako Amir <sabina.diako@gmail.com>
2020-02-24 00:44:37 +0300

    add loghook

--- domain/managing/migrate.go
+++ domain/managing/migrate.go
@@ -1,15 +1,11 @@
 package managing
 
 import (
-	"fmt"
 	"sigma/domain/core/engine"
 	"sigma/domain/managing/model"
 )
 
-// import
-
 // Migrate is called for creating tables, indexes and etc
 func Migrate(e *engine.Engine) {
 	e.DB.AutoMigrate(&model.Role{})
-
 }
